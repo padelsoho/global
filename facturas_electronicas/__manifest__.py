893f8f7237eae09ccec83de693f75b4873488a09
padelsoho <odoo@ampd1.odoo.com>
2022-05-25 01:10:20 +0000

    Enlaces Instaladores Modulos

--- facturas_electronicas/__manifest__.py
+++ facturas_electronicas/__manifest__.py
@@ -9,8 +9,8 @@
         Long description of module's purpose
     """,
 
-    'author': "JC",
-    'website': "http://www.demo.com",
+    'author': "THAVAS",
+    'website': 'website': "http://www.thavasconsultoria.com/odoo",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
